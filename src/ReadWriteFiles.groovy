2b7aa800a2969cad1563f97884687852e3704916
Sheikh Rahmatulla Sakib <kishorsakib10@gmail.com>
2023-03-27 11:37:42 +0600

    initial

--- src/ReadWriteFiles.groovy
+++ src/ReadWriteFiles.groovy
@@ -55,5 +55,35 @@ class ReadWriteFiles {
         new File("/home/kishor/IdeaProjects").eachFile {files-> println(files.getAbsolutePath())}
         //recursively display all files in a directory & its subdirectory
         new File ("/home/kishor/IdeaProjects").eachFileRecurse {file-> println file.getAbsolutePath()}
+
+        //copy file data to another file
+        def newFile=new File("data3.txt")
+        newFile << myFile.text
+        //delete new file
+        newFile.delete()
+
+    //Write Files
+        File myFile2 =new File("data4.txt")
+        myFile2.write("This is Line 1 ")
+        myFile2<<"\nThis is line 2"
+        //.text function over write
+        println(myFile2.text)
+        myFile2.text="\nThis is line 3"
+        println(myFile2.text)
+        //over write again
+        myFile2.withWriter {writer-> writer.writeLine("\4This is line 4")}
+        myFile2.append("\nThis is line 5")
+        println(myFile2.length())
+        println(myFile2.isFile())
+        println(myFile2.isDirectory())
+        println(myFile2.isHidden())
+        //push or copy
+        def newFile2=new File ("data5.txt")
+        newFile2<<myFile2.text
+        //empty my file
+        myFile2.bytes=[]
+        //rename
+        myFile2.renameTo(new File("newFile3.txt"))
+        //println myFile2.delete()
     }
 }
